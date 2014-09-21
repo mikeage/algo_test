f5045643facf609fba118a416914a9de3b43dda8
Mike Miller <github@mikeage.net>
2014-09-21 15:17:54 +0300

    Always enable ERROR and WARNING logs

--- src/log.h
+++ src/log.h
@@ -8,16 +8,12 @@
 // OPTIMIZE: make this work with C++ strings and not just C char* arrays! There are way too many .c_str() littering this code.
 
 #ifdef TRACE 
-
 #define INFO(...) debug_print(LEVEL_INFO, __FILE__, __LINE__, __VA_ARGS__)
+#else
+#define INFO(...) 
+#endif
+
 #define WARNING(...) debug_print(LEVEL_WARNING, __FILE__, __LINE__, __VA_ARGS__)
 #define ERROR(...) debug_print(LEVEL_ERROR, __FILE__, __LINE__, __VA_ARGS__)
+
 void debug_print(int level, const char * file, int line, const char * fmt, ...);
-
-#else
-
-#define INFO(...) 
-#define WARNING(...) 
-#define ERROR(...) 
-
-#endif
